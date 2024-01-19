216a411392cde8f1097aa5d03537cb73276290f1
Aidan Williams <aidanwilliams01@gmail.com>
2024-01-19 13:45:31 -0800

    add home page, migration

--- RecipeBox/Controllers/HomeController.cs
+++ RecipeBox/Controllers/HomeController.cs
@@ -1,14 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using RecipeBox.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
-namespace Library.Controllers
+namespace RecipeBox.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly RecipeBoxContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public HomeController(UserManager<ApplicationUser> userManager, RecipeBoxContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
 
     [HttpGet("/")]
-    public ActionResult Index()
+    public async Task<ActionResult> Index()
     {
-      return View();
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+      if (currentUser != null)
+      {
+        Recipe[] recipes = _db.Recipes
+                    .Where(entry => entry.User.Id == currentUser.Id)
+                    .ToArray();
+        model.Add("recipes", recipes);
+      }
+      return View(model);
     }
   }
 }
